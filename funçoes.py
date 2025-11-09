5badb0ecb14126bdbc0b658c6f5faf51ad665f61
kete <ketelen.lima10@gmail.com>
2025-11-08 23:21:54 -0300

    nov2
    Refactor the travel management system to enhance functionality and readability.

--- funçoes.py
+++ funçoes.py
@@ -1,30 +1,58 @@
-from funcoes import *
-def menu():
-    listaViagens=[]
-    while True:
-        
-        print("biblioteca de livros")
-        print("1- Registra nova viagem")
-        print("2- Exibir de todas viagens") 
-        print("3- Buscar viagem por motoristas")
-        print("4- Exibir viagem mais cara")
-        print("5- Mostrar media geral de consumo") 
-        print("0- Encerra o programa")
+from tabulate import tabulate
+listaViagens=[]
+def registrar_viagem(listaViagens):
+    nome=input("digite o nome do motorista: ")
+    destino=input("digite o seu destino: ")
+    distancia=float(input("digite a sua distancia: "))
+    combustivel=float(input("digite o seu combustivel: "))
+    consumo=combustivel/distancia
+     
+    listaViagens.append ({
+        "nome": nome,
+        "destino": destino,
+        "distancia":distancia,
+        "combustivel":combustivel,
+        "consumo": consumo
+    })
 
-        opçao=input("escolha uma opçao: ") 
+    print(listaViagens)
 
-        if opçao =="1":
-           registrar_viagem(listaViagens)
-        elif  opçao=="2":
-             exibir_viagens(listaViagens)
-        elif opçao=="3" :
-             buscar_motorista(listaViagens)
-        elif opçao=="4" :
-             viagem_mais_cara(listaViagens)
-        elif opçao=="5":  
-             media_consumo(listaViagens)
+def exibir_viagens(listaViagens) :
+    if not listaViagens:
+        print("nao a viagens registradas")
+    else:    
+        tabela = []
+        for a in listaViagens:
+            tabela.append([a["nome"],  a["destino"], a["distancia"],a["combustivel"],a["consumo"]])
 
-        elif opçao=="0":
-            print("fim")
-            break
-menu()        
+            print(" RELATÓRIO DE VIAGENS")
+            print(tabulate(
+                tabela,
+                headers=["nome", "destino", "distancia","combustivel","consumo"],
+                tablefmt="fancy_grid"
+            ))
+def buscar_motorista(listaViagens):
+    nomemot=input("digite nome do motorista: ")
+    viagem=[v for v in listaViagens if v["nome"].lower()==nomemot.lower()]
+    
+    if viagem:
+      print(tabulate(viagem, headers="keys",tablefmt="grid"))
+
+    else:
+      print("nenhuma viagem encontrada")    
+
+def  viagem_mais_cara(listaViagens):
+     if not listaViagens:
+         print("nenhuma")
+         return
+     else:
+        viagem_cara=max(listaViagens,key=lambda v: v["combustivel"])
+        print("viagem cara: ")
+        print(tabulate([viagem_cara], headers="keys",tablefmt="grid"))
+
+def media_consumo(listaViagens):
+    if not listaViagens:
+        print("nenhuma viagem registrada")  
+        return
+    media=sum(v["consumo"] for v in listaViagens)/len(listaViagens)
+    print(f"a media geral de consumo: {media} R$/km")     
